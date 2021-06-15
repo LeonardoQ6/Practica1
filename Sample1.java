2e499a692d5deb8d93ca5a39109c4e7fef60a29b
Leonardo Quintanilla <leonardoqp.6@gmail.com>
2021-06-15 10:30:20 -0600

    Intercambio inicial de proyecto

---
+++ Sample1.java
@@ -0,0 +1,33 @@
+
+/**
+ * Write a description of class Sample1 here.
+ * 
+ * @author (Leonardo) 
+ * @version (1.0)
+ */
+public class Sample1
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class Sample1
+     */
+    public Sample1()
+    {
+        // initialise instance variables
+        x = 5;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+    public int sampleMethod(int y)
+    {
+        // Write a number
+        return x + y;
+    }
+}
